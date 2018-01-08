04990a6360176db9446db9086ceaf6cb4031a073
Szeba25 <lustmord20@gmail.com>
2018-01-08 18:47:01 +0100

    win10 lag fix

--- plugins/NB_GameLoopFix.js
+++ plugins/NB_GameLoopFix.js
@@ -3,9 +3,7 @@
 //=============================================================================
 
 /*:
- * @plugindesc Fixes the game loop to not update in an inner while loop.
- * And fixes the renderer to properly use performance.now(), and made one frame
- * 16 ms, not 15... -.-
+ * @plugindesc Minor fixes to the game loop
  * @author Scalytank
  */
 
@@ -18,13 +16,15 @@
         } else {
             var newTime = this._getTimeInMsWithoutMobileSafari();
             var fTime = (newTime - this._currentTime) / 1000;
-            if (fTime > 0.25) fTime = 0.25;
+            
             this._currentTime = newTime;
             this._accumulator += fTime;
             
-            // Changed from "while" to "if"!
+            if (this._accumulator >= this._deltaTime * 3) {
+                this._accumulator = this._deltaTime * 3;
+            }
             
-            if (this._accumulator >= this._deltaTime) {
+            while (this._accumulator >= this._deltaTime) {
                 this.updateInputData();
                 this.changeScene();
                 this.updateScene();
@@ -36,22 +36,17 @@
     };
     
     Graphics.render = function(stage) {
-        if (this._skipCount === 0) {
-            var startTime = performance.now();
-            if (stage) {
-                this._renderer.render(stage);
-                if (this._renderer.gl && this._renderer.gl.flush) {
-                    this._renderer.gl.flush();
-                }
+        var startTime = performance.now();
+        if (stage) {
+            this._renderer.render(stage);
+            if (this._renderer.gl && this._renderer.gl.flush) {
+                this._renderer.gl.flush();
             }
-            var endTime = performance.now();
-            var elapsed = endTime - startTime;
-            this._skipCount = Math.min(Math.floor(elapsed / 16), this._maxSkip);
-            this._rendered = true;
-        } else {
-            this._skipCount--;
-            this._rendered = false;
         }
+        var endTime = performance.now();
+        var elapsed = endTime - startTime;
+        
+        this._rendered = true;
         this.frameCount++;
     };
     
